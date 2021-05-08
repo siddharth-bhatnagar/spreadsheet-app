62cfe8507ae0269320fb1e135c77342cb7b55433
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-08 17:26:19 +0530

    update

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 const ps = new PerfectScrollbar("#cells", {
-    wheelSpeed: 2,
-    wheelPropagation: true,
-    minScrollbarLength: 20
+    wheelSpeed: 15
 });
-
-for (let i = 1; i <= 100; i++) {
-    let str = "";
-    let n = i;
-
-    while (n > 0) {
-        let rem = n % 26;
-        if (rem == 0) {
-            str = "Z" + str;
-            n = Math.floor(n / 26) - 1;
-        } else {
-            str = String.fromCharCode((rem - 1) + 65) + str;
-            n = Math.floor(n / 26);
-        }
-    }
-    $('#columns').append(`<div class="column-name">${str}</div>`);
-    $('#rows').append(`<div class="row-name">${i}</div>`)
-}
 
 for (let i = 1; i <= 100; i++) {
     let str = "";
@@ -48,17 +28,18 @@ for (let i = 1; i <= 100; i++) {
     $("#cells").append(row);
 }
 
-$("#cells").scroll(function (e) {
+$("#cells").scroll(function(e) {
     $("#columns").scrollLeft(this.scrollLeft);
     $("#rows").scrollTop(this.scrollTop);
 });
 
-$(".input-cell").dblclick(function (e) {
+$(".input-cell").dblclick(function(e) {
+    $(".input-cell.selected").removeClass("selected top-selected bottom-selected left-selected right-selected");
     $(this).attr("contenteditable", "true");
     $(this).focus();
 });
 
-$(".input-cell").blur(function (e) {
+$(".input-cell").blur(function(e) {
     $(this).attr("contenteditable", "false");
 });
 
@@ -67,91 +48,93 @@ function getRowCol(ele) {
     let idArray = id.split("-");
     let rowId = parseInt(idArray[1]);
     let colId = parseInt(idArray[3]);
-    return [rowId, colId];
+    return [rowId,colId];
 }
 
-function getTopLeftBottomRightCell(rowId, colId) {
+function getTopLeftBottomRightCell(rowId,colId) {
     let topCell = $(`#row-${rowId - 1}-col-${colId}`);
     let bottomCell = $(`#row-${rowId + 1}-col-${colId}`);
     let leftCell = $(`#row-${rowId}-col-${colId - 1}`);
     let rightCell = $(`#row-${rowId}-col-${colId + 1}`);
-    return [topCell, bottomCell, leftCell, rightCell];
+    return [topCell,bottomCell,leftCell,rightCell];
 }
-$(".input-cell").click(function (e) {
-    let [rowId, colId] = getRowCol(this);
-    let [topCell, bottomCell, leftCell, rightCell] = getTopLeftBottomRightCell(rowId, colId);
-    if ($(this).hasClass("selected") && e.ctrlKey) {
-        unselectCell(this, e, topCell, bottomCell, leftCell, rightCell);
+$(".input-cell").click(function(e) {
+    let [rowId,colId] = getRowCol(this);
+    let [topCell,bottomCell,leftCell,rightCell] = getTopLeftBottomRightCell(rowId,colId);
+    if($(this).hasClass("selected") && e.ctrlKey) {
+        unselectCell(this,e,topCell,bottomCell,leftCell,rightCell);
     } else {
-        selectCell(this, e, topCell, bottomCell, leftCell, rightCell);
+        selectCell(this,e,topCell,bottomCell,leftCell,rightCell);
     }
 });
 
-function unselectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
-    if ($(ele).hasClass("top-selected")) {
-        topCell.removeClass("bottom-selected");
+function unselectCell(ele,e,topCell,bottomCell,leftCell,rightCell) {
+    if($(ele).attr("contenteditable") == "false") {
+        if($(ele).hasClass("top-selected")) {
+            topCell.removeClass("bottom-selected");
+        }
+    
+        if($(ele).hasClass("bottom-selected")) {
+            bottomCell.removeClass("top-selected");
+        }
+    
+        if($(ele).hasClass("left-selected")) {
+            leftCell.removeClass("right-selected");
+        }
+    
+        if($(ele).hasClass("right-selected")) {
+            rightCell.removeClass("left-selected");
+        }
+    
+        $(ele).removeClass("selected top-selected bottom-selected left-selected right-selected");
     }
-
-    if ($(ele).hasClass("bottom-selected")) {
-        bottomCell.removeClass("top-selected");
-    }
-
-    if ($(ele).hasClass("left-selected")) {
-        leftCell.removeClass("right-selected");
-    }
-
-    if ($(ele).hasClass("right-selected")) {
-        rightCell.removeClass("left-selected");
-    }
-
-    $(ele).removeClass("selected top-selected bottom-selected left-selected right-selected");
 
 }
 
-function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
-    if (e.ctrlKey) {
+function selectCell(ele,e,topCell,bottomCell,leftCell,rightCell) {
+    if(e.ctrlKey) {
 
         // top selected or not
         let topSelected;
-        if (topCell) {
+        if(topCell) {
             topSelected = topCell.hasClass("selected");
         }
 
         // bottom selected or not
         let bottomSelected;
-        if (bottomCell) {
+        if(bottomCell) {
             bottomSelected = bottomCell.hasClass("selected");
         }
 
 
         // left selected or not
         let leftSelected;
-        if (leftCell) {
+        if(leftCell) {
             leftSelected = leftCell.hasClass("selected");
         }
 
         // right selected or not
         let rightSelected;
-        if (rightCell) {
+        if(rightCell) {
             rightSelected = rightCell.hasClass("selected");
         }
 
-        if (topSelected) {
+        if(topSelected) {
             $(ele).addClass("top-selected");
             topCell.addClass("bottom-selected");
         }
-
-        if (bottomSelected) {
+        
+        if(bottomSelected) {
             $(ele).addClass("bottom-selected");
             bottomCell.addClass("top-selected");
         }
 
-        if (leftSelected) {
+        if(leftSelected) {
             $(ele).addClass("left-selected");
             leftCell.addClass("right-selected");
         }
 
-        if (rightSelected) {
+        if(rightSelected) {
             $(ele).addClass("right-selected");
             rightCell.addClass("left-selected");
         }
